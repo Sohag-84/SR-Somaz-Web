8123b2e594e1af5f6cc21d4e90815b325805b3d7
Sohag-84 <ihsohag84@gmail.com>
2024-06-25 09:31:45 +0600

    designed web appbar

--- lib/pages/web_home_page.dart
+++ lib/pages/web_home_page.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class WebHomePage extends StatelessWidget {
   const WebHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(
+            "assets/photos/logo.png",
+            width: 30,
+          ),
+        ),
+        title: const Text(
+          'SAMAPRK',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.download),
+            label: Text(
+              'Donwload',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+          ),
+          const Gap(10),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "SR SOMAZ",
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: theme.colorScheme.onSurface,
+                      ),
+                    ),
+                    const Text("The best app connecting with"),
+                    const Text("Your friends and family members"),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
